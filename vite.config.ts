e3017a7aa3e0af5b48dd9837eb368c3e0ed7996d
Aurélien Dudonney <a.dudonney@gmail.com>
2022-02-03 18:25:23 +0100

    rename dist folder into doc folder to follow Github requirement

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'doc',
     rollupOptions: {
       output: {
         chunkFileNames: `assets/[name].js`,
