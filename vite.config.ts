3f807d5ef366746cb6ac5fca97d8fc9b7f8adfcd
Aurélien Dudonney <a.dudonney@gmail.com>
2022-02-03 18:58:02 +0100

    fix some problems

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/demoReactSW/',
   build: {
     outDir: 'docs',
+    manifest: true,
     rollupOptions: {
       output: {
         chunkFileNames: `assets/[name].js`,
