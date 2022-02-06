97261b05f86a270f833e0996a96f83ad1ddb4743
Aurélien Dudonney <a.dudonney@gmail.com>
2022-02-06 22:03:20 +0100

    improve loading component

--- src/views/components/spinner.tsx
+++ src/views/components/spinner.tsx
@@ -4,9 +4,13 @@ import React, { FunctionComponent } from 'react'
  * Spinner
  */
 const Spinner:FunctionComponent = () => (
-  <div className="flex justify-center items-center">
-    <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-      <span className="visually-hidden">Loading...</span>
+  <div className="h-screen w-screen flex bg-slate-50">
+    <div className="flex justify-center items-center m-auto">
+      <div className="spinner-border animate-spin inline-block w-10 h-10 border-t-4 border-l-4 border-sky-600 rounded-full" role="status">
+        <span className="sr-only">
+          Loading...
+        </span>
+      </div> 
     </div>
   </div>
 )
