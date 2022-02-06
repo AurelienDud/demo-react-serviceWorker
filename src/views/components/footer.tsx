ea429ed2c39d6c5fa2683908d17332b4ef33f805
Aurélien Dudonney <a.dudonney@gmail.com>
2022-02-06 22:16:04 +0100

    Update title and footer

--- src/views/components/footer.tsx
+++ src/views/components/footer.tsx
@@ -5,7 +5,9 @@ const Footer:FunctionComponent = () => {
     <div className='p-5 mt-5 text-center text-white bg-gradient-to-r from-zinc-800 to-slate-700'>
       © 2022 - Aurélien Dudonney
       <br />
-      Thanks to <a className="underline" href="https://picsum.photos/" target="_blank" rel="noreferrer">Picsum</a>
+      Build quickly with <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">ViteJs</a>
+      <br />
+      Imaged thanks to <a className="underline" href="https://picsum.photos/" target="_blank" rel="noreferrer">Picsum</a>
     </div>
   )
 }
